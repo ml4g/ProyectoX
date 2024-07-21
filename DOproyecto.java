fdfc04909b47b896fb540bc7a6a379fe4bb3901d
ml4g <m.laguna@ebc.edu.mx>
2024-07-21 16:14:49 -0600

    Proyecto Fase 1

---
+++ DOproyecto.java
@@ -0,0 +1,11 @@
+public class DOproyecto {
+
+    public static void main(String[] args) {
+        // Mensaje a imprimir
+        String mensaje = "HOLA MUNDO!!";
+
+        // Imprimir el mensaje en la consola
+        System.out.println(mensaje);
+    }
+    
+}
